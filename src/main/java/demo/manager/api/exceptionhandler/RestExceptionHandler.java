990afbfc6a876ca6e5078edd597e6c3649be716d
ramonptz <89748744+ramonptz@users.noreply.github.com>
2023-12-28 12:43:13 -0300

    new handlerException start

---
+++ src/main/java/demo/manager/api/exceptionhandler/RestExceptionHandler.java
@@ -0,0 +1,21 @@
+package demo.manager.api.exceptionhandler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class RestExceptionHandler{
+	
+	@ResponseBody
+	@ExceptionHandler(PessoaNotFound.class)
+	public ResponseEntity<MessageExceptionHandler> PessoaNotFound(PessoaNotFound pessoaNotFound){
+		MessageExceptionHandler error = new MessageExceptionHandler(new Date(), HttpStatus.NOT_FOUND.value(), "Pessoa Não Encontrada");
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+
+}
