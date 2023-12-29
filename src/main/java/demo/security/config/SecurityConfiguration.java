70687a77f3ef05fa24ab3e3ae6d2822bdc049e6a
Ramon <ramonboladao98@gmail.com>
2023-12-28 23:28:53 -0300

    Update SecurityConfiguration.java

--- src/main/java/demo/security/config/SecurityConfiguration.java
+++ src/main/java/demo/security/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package demo.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
         .csrf(csrf -> csrf.disable())
