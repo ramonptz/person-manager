8dc2a650f17fe094fa315c719ebcd8bf2124045f
Ramon <ramonboladao98@gmail.com>
2023-12-28 23:57:07 -0300

    criando metodo de autenticacao

--- src/main/java/demo/security/config/SecurityConfiguration.java
+++ src/main/java/demo/security/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package demo.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,8 +14,12 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
-        .csrf(csrf -> csrf.disable())
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .build();
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.POST, "/pessoa/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+            )
+                .build();
     }
 }
---
+++ src/main/java/demo/security/controllers/AuthenticationController.java
@@ -0,0 +1,17 @@
+package demo.security.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+    
+    // @PostMapping("/login")
+    // public ResponseEntity login(@RequestBody @Valid Authentication){
+
+    // }
+
+}
